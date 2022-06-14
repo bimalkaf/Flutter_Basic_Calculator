aa8d612c7e954d04f2e59c00e4d201c8e9950717
bimalkaf <bimalisnotbimal@gmail.com>
2022-06-14 21:50:46 +0530

    initial commit

---
+++ lib/calculator.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class Calculator extends StatefulWidget {
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  String statement = "";
+  String result = "0";
+  final List<String> buttons = [
+    'C',
+    '(',
+    ')',
+    '/',
+    '7',
+    '8',
+    '9',
+    '*',
+    '4',
+    '5',
+    '6',
+    '+',
+    '1',
+    '2',
+    '3',
+    '-',
+    'AC',
+    '0',
+    '.',
+    '=',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Flexible(flex: 2, child: resultWidget()),
+            Expanded(flex: 4, child: _buttons()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget resultWidget() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(20),
+          alignment: Alignment.centerRight,
+          child: FittedBox(
+            child: Text(
+              statement,
+              style: const TextStyle(fontSize: 32),
+            ),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(15),
+          alignment: Alignment.centerRight,
+          child: FittedBox(
+            child: Text(
+              result,
+              style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buttons() {
+    return GridView.builder(
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+      itemBuilder: (BuildContext context, int index) {
+        return _myButton(buttons[index]);
+      },
+      itemCount: buttons.length,
+    );
+  }
+
+  _myButton(String text) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: MaterialButton(
+        onPressed: () {
+          setState(() {
+            handleButtonTap(text);
+          });
+        },
+        color: _getColor(text),
+        textColor: Colors.white,
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 24),
+        ),
+        shape: const CircleBorder(),
+      ),
+    );
+  }
+
+  handleButtonTap(String text) {
+    if (text == "AC") {
+      statement = "";
+      result = "0";
+      return;
+    }
+    if (text == "=") {
+      result = calculate();
+      if (result.endsWith(".0")) {
+        result = result.replaceAll(".0", "");
+      }
+      return;
+    }
+
+    if (text == "C") {
+      statement = statement.substring(0, statement.length - 1);
+      return;
+    }
+    statement = statement + text;
+  }
+
+  calculate() {
+    try {
+      var exp = Parser().parse(statement);
+      var evaluation = exp.evaluate(EvaluationType.REAL, ContextModel());
+      return evaluation.toString();
+    } catch (e) {
+      return "Err";
+    }
+  }
+
+  _getColor(String text) {
+    if (text == "/" || text == "*" || text == "+" || text == "-") {
+      return Colors.orangeAccent;
+    }
+    if (text == "C" || text == "AC") {
+      return Colors.red;
+    }
+    if (text == "(" || text == ")") {
+      return Colors.blueGrey;
+    }
+    return Colors.blueAccent;
+  }
+}
